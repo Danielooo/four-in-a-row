b5afe5634891c81ce4efeec69b2dc42121d0b1ac
Danielooo <19654989+Danielooo@users.noreply.github.com>
2025-01-02 23:55:31 +0100

    drop_chip works

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+player_sign = 1
+computer_sign = 2
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def player_move(pos_x):
+    drop_chip(pos_x)
+
+
+def drop_chip(pos_x):
+    pos_y = get_lowest_empty_y(pos_x)
+    grid[pos_y][pos_x] = player_sign
+
+
+def get_lowest_empty_y(pos_x):
+    for pos_y in range(5, -1, -1):
+        if grid[pos_y][pos_x] == 0:
+            return pos_y
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Access the element at row 2, column 3
+    # element = grid[2][3]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    grid = [[0 for _ in range(7)] for _ in range(6)]
+
+    # Print the grid
+    for row in grid:
+        print(row)
+
+    print()
+
+    grid[5][0] = 1
+
+    player_move(0)
+
+    for row in grid:
+        print(row)
