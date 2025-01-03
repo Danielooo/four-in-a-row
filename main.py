bd87a80cd5db82361fad746ddc7759383958b2bd
Danielooo <19654989+Danielooo@users.noreply.github.com>
2025-01-03 16:18:36 +0100

    added row win condition check

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from array import array
 
 player_sign = 1
 computer_sign = 2
@@ -18,6 +19,31 @@ def get_lowest_empty_y(pos_x):
             return pos_y
 
 
+def print_grid():
+    for row in grid:
+        print(row)
+    print()
+
+
+def is_win_condition_met():
+    for row in grid:
+        if check_four_in_a_row(row):
+            return True
+    return False
+
+
+def check_four_in_a_row(row):
+    count = 1
+    for i in range(1, len(row)):
+        if row[i] and row[i] == row[i - 1]:
+            count += 1
+            if count == 4:
+                return True
+        else:
+            count = 1
+    return False
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Access the element at row 2, column 3
@@ -26,15 +52,27 @@ if __name__ == '__main__':
     grid = [[0 for _ in range(7)] for _ in range(6)]
 
     # Print the grid
-    for row in grid:
-        print(row)
+    print_grid()
 
-    print()
+    # grid[5][0] = 2
+    player_input = int(input("Enter your x position (0-6): "))
+    player_move(player_input)
+    print_grid()
+    if is_win_condition_met(): print("Win condition met!")
 
-    grid[5][0] = 1
 
     player_input = int(input("Enter your x position (0-6): "))
     player_move(player_input)
+    print_grid()
+    if is_win_condition_met(): print("Win condition met!")
 
-    for row in grid:
-        print(row)
+    player_input = int(input("Enter your x position (0-6): "))
+    player_move(player_input)
+    print_grid()
+    if is_win_condition_met(): print("Win condition met!")
+
+    player_input = int(input("Enter your x position (0-6): "))
+    player_move(player_input)
+    print_grid()
+    if is_win_condition_met(): print("Win condition met!")
+
