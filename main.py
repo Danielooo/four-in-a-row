9148e8d2ef9c8d608a6e0adedf9b504d0ea6b206
Danielooo <19654989+Danielooo@users.noreply.github.com>
2025-01-05 21:41:34 +0100

    added computer turn

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from win_conditions import is_win_condition_met
 
 player_sign = 1
 computer_sign = 2
+players_turn = True
 
 def player_move(pos_x):
     drop_chip(pos_x)
 
 def drop_chip(pos_x):
     pos_y = get_lowest_empty_y(pos_x)
-    grid[pos_y][pos_x] = player_sign
+    grid[pos_y][pos_x] = player_sign if players_turn else computer_sign
 
 def get_lowest_empty_y(pos_x):
     for pos_y in range(5, -1, -1):
@@ -21,6 +22,11 @@ def print_grid():
         print(row)
     print()
 
+
+def computer_move():
+    drop_chip(3)
+
+
 if __name__ == '__main__':
     grid = [[0 for _ in range(7)] for _ in range(6)]
 
@@ -29,12 +35,19 @@ if __name__ == '__main__':
     print_grid()
 
     while(True):
-        player_input = int(input("Enter your x position (0-6): "))
-        player_move(player_input)
+        if players_turn:
+            player_input = int(input("Enter your x position (0-6): "))
+            player_move(player_input)
+        else:
+            computer_move()
+        
         print_grid()
         if is_win_condition_met(grid): 
             print("Win condition met!") 
             break
+
+        players_turn = not players_turn
+        print("Player's turn", players_turn)
 
 
 
